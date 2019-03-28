e51554f57ef4aae1574520a3b54d05bcf3eaaa83
Ludwik <ludwik.kazmierczak@gmail.com>
2019-03-28 14:05:10 +0100

    ups

--- teams/core-platform.js
+++ teams/core-platform.js
@@ -2,18 +2,6 @@ module.exports = {
 	channel: 'G2K4H1CM9',
 	candidates: [
 		// Grunny
-		'<@U02P59UTL>',
-		// Mech
-		'<@U2JJESJGG>',
-		// Ludwik
-		'<@U033EL9H9>',
-		// Martyna
-		'<@UBYKPEB6J>',
-		// Justyna
-		'<@U2JH0AZSB>',
-		// Przemek
-		'<@UF4BWHYN7>',
-		// Harnaś
-		'<@U03H7DH9B>'
+		'<@U02P59UTL>'
 	]
 };
